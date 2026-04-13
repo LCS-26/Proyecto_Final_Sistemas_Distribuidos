bd12992d00507b4589fef39a75b348a014d28434
miriam <202104686@alu.comillas.edu>
2026-04-13 11:36:44 +0200

    Cambio para que funcione el evento sísmico

--- sensor-movement-service/src/main/java/org/toolset/grupo1/movement/SensorMovementApplication.java
+++ sensor-movement-service/src/main/java/org/toolset/grupo1/movement/SensorMovementApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"org.toolset.grupo1.movement", "org.toolset.grupo1.seismic"})
 @EnableAsync
 public class SensorMovementApplication {
 
